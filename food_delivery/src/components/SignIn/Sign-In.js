6ebd7ce3204a81b7e103b1939b6b81d763dcfe5c
Pyry Viirret <pyry.viirret@gmail.com>
2021-11-10 09:01:39 +0200

    CSS fixes

--- food_delivery/src/components/SignIn/Sign-In.js
+++ food_delivery/src/components/SignIn/Sign-In.js
@@ -5,11 +5,11 @@ class SignIn extends React.Component {
     render () {
         return (
             <div className={styles.SignIn}>
-                <h1>Log In</h1>
-                <input type="text" placeholder="Username" style={{borderRadius: '15px', height: '30px'}}/>
+                <h1 style={{marginLeft: '260px'}}>Log In</h1>
+                <input type="text" placeholder="Username" style={{borderRadius: '15px', height: '30px', marginLeft: '220px'}}/>
                 <div style={{marginTop: '20px'}}></div>
-                <input type="text" placeholder="Password" style={{borderRadius: '15px', height: '30px'}}/>
-                <button style={{display:'flex', width:'400px', height:"40px", justifyContent:'center', alignItems:'center', backgroundColor: 'crimson', borderRadius:'15px', marginTop: '20px', marginLeft: '17.5%', marginBottom: '20px'}}>Sign In</button>
+                <input type="text" placeholder="Password" style={{borderRadius: '15px', height: '30px', marginLeft: '220px'}}/>
+                <button style={{display:'flex', width:'400px', height:"40px", justifyContent:'center', alignItems:'center', backgroundColor: '#cc2255', borderRadius:'15px', marginTop: '20px', marginLeft: '17.5%', marginBottom: '20px'}}>Sign In</button>
             </div>
         );
     }
--- food_delivery/src/components/SignUpCustomer/Sign-Up-Customer.js
+++ food_delivery/src/components/SignUpCustomer/Sign-Up-Customer.js
@@ -5,21 +5,21 @@ class SignUpCustomer extends React.Component {
     render () {
         return (
             <div className={styles.SignUpCustomer}>
-                <h1>Sign Up</h1>
-                <input type="text" placeholder="Username" style={{borderRadius: '15px', height: '30px'}}/>
+                <h1 style={{marginLeft: '250px'}}>Sign Up</h1>
+                <input type="text" placeholder="Username" style={{borderRadius: '15px', height: '30px', marginLeft: '220px'}}/>
                 <div style={{marginTop: '20px'}}></div>
 
-                <input type="text" placeholder="Last Name" style={{borderRadius: '15px', height: '30px'}}/>
+                <input type="text" placeholder="Last Name" style={{borderRadius: '15px', height: '30px', marginLeft: '220px'}}/>
                 <div style={{marginTop: '20px'}}></div>
 
-                <input type="text" placeholder="Email" style={{borderRadius: '15px', height: '30px'}}/>
+                <input type="text" placeholder="Email" style={{borderRadius: '15px', height: '30px', marginLeft: '220px'}}/>
                 <div style={{marginTop: '20px'}}></div>
 
-                <input type="text" placeholder="Phone Number" style={{borderRadius: '15px', height: '30px'}}/>
+                <input type="text" placeholder="Phone Number" style={{borderRadius: '15px', height: '30px', marginLeft: '220px'}}/>
                 <div style={{marginTop: '20px'}}></div>
 
-                <input type="text" placeholder="Password" style={{borderRadius: '15px', height: '30px'}}/>
-                <button style={{display:'flex', width:'400px', height:"40px", justifyContent:'center', alignItems:'center', backgroundColor: 'crimson', borderRadius:'15px', marginTop: '20px', marginLeft: '17.5%', marginBottom: '40px'}}>Register</button>
+                <input type="text" placeholder="Password" style={{borderRadius: '15px', height: '30px', marginLeft: '220px'}}/>
+                <button style={{display:'flex', width:'400px', height:"40px", justifyContent:'center', alignItems:'center', backgroundColor: '#cc2255', borderRadius:'15px', marginTop: '20px', marginLeft: '17.5%', marginBottom: '40px'}}>Register</button>
             </div>
         );
     }
