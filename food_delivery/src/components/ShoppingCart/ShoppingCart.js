81effe85c06e67e381bdc024b72a8f0b7a40b8c2
Saana Pullola <t0pusa00@students.oamk.fi>
2021-11-11 15:24:31 +0200

    checkout button with a link added to shopping cart UI

--- food_delivery/src/components/ShoppingCart/ShoppingCart.js
+++ food_delivery/src/components/ShoppingCart/ShoppingCart.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from './ShoppingCart.module.css'
 import ShoppingCartItems from './ShoppingCartItems';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 class ShoppingCart extends React.Component {
     render () {
         return (
+            <div>
             <div className = {styles.column}>                                       
             <div className = {styles.topBar}>
             Your orders
@@ -22,6 +24,8 @@ class ShoppingCart extends React.Component {
                <div style={{marginTop: "50px", fontSize: "18px"}}>Total</div>
                <div style={{marginBottom: "25px", fontSize: "20px", fontWeight: "500"}}>50.00€</div>
             </div>
+            </div>
+            <Link to="/payment" style={{textDecoration: 'none'}}><div className = {styles.checkout}>Checkout</div></Link>  {/*Checkout 'button' to the payment page*/}
             </div>
         );
     }
