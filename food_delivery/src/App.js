8888085473d4afdf3b2961606653423e9cabcfd1
Saana Pullola <t0pusa00@students.oamk.fi>
2021-11-19 10:45:44 +0200

    icons added, simple account info page made

--- food_delivery/src/App.js
+++ food_delivery/src/App.js
@@ -7,12 +7,9 @@ import MenuPage from './components/MenuPage/MenuPage';
 import CreateRestaurant from './components/CreateRestaurant/CreateRestaurant';
 import CustomerOrders from './components/CustomerOrders/CustomerOrders';
 import ShoppingCart from "./components/ShoppingCart/ShoppingCart"
-<<<<<<< HEAD
 import CustomerInfoPage from './components/CustomerInfoPage/CustomerInfoPage';
-=======
 import CreateMenu from "./components/CreateMenu/CreateMenu"
 import RestaurantOrders from './components/RestaurantOrders/RestaurantOrders';
->>>>>>> e68f7db4737691eddd283a967a10916bbc1e9d7d
 import React from 'react';
 import data from './restaurants.json';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
@@ -48,7 +45,7 @@ return (
      (restaurants) => restaurants.name.includes(this.state.itemSearchString))}/>  } />
         <Route path="/register" element={<SignUpCustomer/>} />
         <Route path="/createrestaurant" element={<CreateRestaurant/>}/>
-        <Route path="/login" element={<RestaurantOrders/>} />  {/* here should be SignIn */}
+        <Route path="/login" element={<SignIn/>} />  {/* here should be SignIn */}
         <Route path="/menupages" element={<MenuPage restaurants={this.state.restaurants}/>} />
         <Route path="/orders" element={<CustomerOrders/>} />
         <Route path="/shoppingcart" element={<ShoppingCart/>} /> 
--- food_delivery/src/components/Header/Header.js
+++ food_delivery/src/components/Header/Header.js
@@ -22,27 +22,22 @@ class Header extends React.Component {
             <Link to="/" style={{textDecoration: 'none'}}><div style={{marginRight: '300px', marginLeft: '20px', color: 'white'}}>YammyGo</div></Link>
             <input type="text" placeholder="Search restaurants..." className={styles.searchBar} onChange={this.props.onSearchChange} value={this.props.itemSearchString}/>
             <button className={styles.searchButton}> Search </button>
-            <Link to="/login" style={{textDecoration: 'none'}}><div style={{marginRight: '50px', color: 'white'}}>Log in</div></Link>
-            <Link to="/register" style={{textDecoration: 'none'}}><div style={{marginRight: '50px', color: 'white'}}>Register</div></Link>
-<<<<<<< HEAD
+            <Link to="/login" style={{textDecoration: 'none'}}><div className={styles.text}>
+                <img src = '/images/user.png' className = {styles.icon}/>Log in</div></Link>
+            <Link to="/register" style={{textDecoration: 'none'}}><div className={styles.text}>
+            <img src = '/images/register.png' className = {styles.icon}/>Register</div></Link>
             <Link to="/shoppingcart" className = {styles.buttonContainer}>
             <button className = {styles.shoppingcartButton}>
-            <img className = {styles.icon} src = "images/Cart.png"></img>
+            <img className = {styles.shoppingIcon} src = "images/Cart.png"></img>
             <div style = {{fontSize: '16px'}}>
                 Empty 
             </div>
             </button></Link>             
-=======
             
-            <Link to="/orders" style={{textDecoration: 'none',backgroundColor: '#cc2255',textAlign: 'center',
-    fontSize: '16px', height: '100%',width: '15%', marginLeft: 'auto'}}>
-                <button className={styles.button}>Orders</button></Link>
-
-            
-            
+           {/*<Link to="/orders" style={{textDecoration: 'none',backgroundColor: '#cc2255',textAlign: 'center',
+                fontSize: '16px', height: '100%',width: '15%', marginLeft: 'auto'}}>
+                <button className={styles.button}>Orders</button></Link>*/}         
            
-
->>>>>>> e68f7db4737691eddd283a967a10916bbc1e9d7d
         </div>
         );
     }
