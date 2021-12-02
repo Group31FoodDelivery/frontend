2b463dbe4c42fc22f31f230d3a52001a77a3e123
Diana <c0sydi00@students.oamk.fi>
2021-12-02 15:02:21 +0200

    balbla

--- food_delivery/src/app/store.js
+++ food_delivery/src/app/store.js
@@ -1,5 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose,combineReducers } from 'redux';
 
-export default configureStore({
-  reducer: {},
-})
+
+const initialState = {};
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  combineReducers({
+    products: {},
+    cart: {},
+    order: {},
+  }),
+  initialState,
+  composeEnhancer(applyMiddleware(thunk))
+);
+export default store;
---
+++ food_delivery/src/features/counter/counterSlice.js
@@ -0,0 +1,20 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+// interface CounterState{
+//     value:number;
+// }
+// const initialSatate : CounterState ={
+//     value:0,
+// };
+
+// const counterSlice = createSlice({
+//     name: 'counter',
+//     initialiState,
+//     reducer:{
+// incremented (state){
+// state.value ++;
+// },
+//     }
+// });
+
+// export const{  incromented} = 
