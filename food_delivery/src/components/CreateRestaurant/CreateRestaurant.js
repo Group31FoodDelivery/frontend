da452641bedcd1b8a4400f6212e91a8580034cc0
HenriV <henri.vaaraniemi@hotmail.fi>
2021-12-03 15:28:22 +0200

    headeria ja create restauranttia

--- food_delivery/src/components/CreateRestaurant/CreateRestaurant.js
+++ food_delivery/src/components/CreateRestaurant/CreateRestaurant.js
@@ -6,7 +6,12 @@ class CreateRestaurant extends React.Component {
         super();
         this.state = { 
             type: "fine_dining", 
-            priceLevel: "1"};
+            priceLevel: "1",
+            name: null,
+            description: null,
+            operatingHours: null,
+            address: null
+        }
         this.handleChange = this.handleChange.bind(this);
         this.handleChange2 = this.handleChange2.bind(this);
     }
@@ -19,12 +24,26 @@ class CreateRestaurant extends React.Component {
         this.setState({ priceLevel: event.target.value});
     }
 
+    handleRestaurant = async e => {
+       if (this.state.name && this.state.address && this.state.operatingHours && this.state.description){
+
+        console.log("Tähän lisää");
+
+       }
+       else{
+           console.log("ei ollu kaikki laitettu")
+       }
+
+    }
+
+
     render() {
         return (
             <div>
             <div className = {styles.title}><h1>Create Restaurant</h1></div>
             <div className = {styles.topBar}>Add a new restaurant</div>
-            <div className={styles.CreateRestaurant}>
+            <form className={styles.CreateRestaurant} onSubmit={this.handleRestaurant}>
+            
                 <div style = {{marginTop: '10px'}}>Restaurant type</div>
                 <select value={this.state.type} onChange={this.handleChange} className = {styles.select} style = {{marginTop: '10px'}} >
                     <option name="fine_dining">Fine Dining</option>
@@ -42,17 +61,21 @@ class CreateRestaurant extends React.Component {
                     <option name="5">5</option>
                 </select>
 
-                <input type="text" placeholder="Name" className = {styles.inputs}/>
+                <input type="text" placeholder="Name" className = {styles.inputs} onChange={e => this.setState({name: e.target.value})}/>
 
-                <textarea type="text" placeholder="Description" cols="40"  rows="5" 
-               className = {styles.descriptionInputs}></textarea>
+               {/* <input type="text" placeholder="Description" cols="40"  rows="5" 
+               className = {styles.descriptionInputs} onChange={e => this.setState({description: e.target.value})}></input> */}
 
-                <input type="text" placeholder="Operating hours"  className = {styles.inputs}/>
+                <input type="text" placeholder="Description"  className = {styles.inputs} onChange={e => this.setState({description: e.target.value})}/>
 
-                <textarea  type="text" placeholder="Address"  className = {styles.addressInputs}></textarea>
+                <input type="text" placeholder="Operating hours"  className = {styles.inputs} onChange={e => this.setState({operatingHours: e.target.value})}/>
+                <input type="text" placeholder="Address"  className = {styles.inputs} onChange={e => this.setState({address: e.target.value})}/>
+
+                {/*<input  type="text" placeholder="Address"  className = {styles.addressInputs} onChange={e => this.setState({address: e.target.value})} > </input> */}
                 
-                <button className = {styles.button}>Create</button>
-            </div>
+                <button type="submit" className = {styles.button}>Create</button>
+            
+            </form>
             </div>
         );
     }
