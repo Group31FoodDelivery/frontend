53f0bdf8b472ad38435513e7214b1ef04d674595
Saana Pullola <t0pusa00@students.oamk.fi>
2021-12-08 15:40:53 +0200

    orderId passing changed

--- food_delivery/src/components/PaymentPage/PaymentPage.js
+++ food_delivery/src/components/PaymentPage/PaymentPage.js
@@ -12,12 +12,8 @@ export default function PaymentPage(props){
 
     const [itemId, setItemId] = useState('')
     const [qnty, setQnty] = useState( )
-    const [orderId, setOrderId] = useState('')
 
     useEffect(() => {
-
-        console.log("orderId")
-        console.log(orderId)
        
        let itemId = cart.map(x => x.itemId)
        setItemId(itemId)
@@ -43,15 +39,13 @@ export default function PaymentPage(props){
         address: address,
         price: price,
         state: 'Pending',
-        timestamp: timeStamp,
-        items: cart
-        
+        timestamp: timeStamp
    
     })
     .then(function (response) {
         console.log(response.data);
-        setOrderId(response.data);
-        placeItemsIntoOrder();
+        let orderId = response.data;
+        placeItemsIntoOrder(orderId);
 
     })
     .catch(function (error){
@@ -62,18 +56,18 @@ export default function PaymentPage(props){
 
 
     //set menuitems into the order
-    const placeItemsIntoOrder = () => {
+    const placeItemsIntoOrder = (orderId) => {
 
         console.log("hello??")
         console.log(orderId);
 
         let itemid = 'ffc1fe01-30f5-4810-9090-e23f23bc96a3'
-        let orderid =  'efb6125e-4e0d-48d4-9513-7e48861bc994'
+        //let orderid =  'efb6125e-4e0d-48d4-9513-7e48861bc994'
 
         axios.post('http://localhost:9000/AddOrderItems' , {
             itemId: itemid,
             orderId: orderId,
-            amount: 3   
+            amount: 4  
         })
         .then(function (response) {
             console.log("responssi")
