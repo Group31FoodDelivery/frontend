9d23538a0a0a064a4b1f13633d1cfe433e61677b
Diana <c0sydi00@students.oamk.fi>
2021-12-09 14:22:38 +0200

    btn created

--- food_delivery/src/components/RestaurantOrders/StatusButton.js
+++ food_delivery/src/components/RestaurantOrders/StatusButton.js
@@ -3,10 +3,18 @@ import styles from './StatusButton.module.css';
 
 export default function StatusButton(props){
 
+function Prepared(){
+    var v  = document.getElementById('buttonB').getAttribute("onClick");
+    setTimeout(v,0);
+}
     return(
         <div>
            
-     <button className={styles.btn1}>  </button>
+     <button className={styles.btn1} onClick={Prepared}>Recived</button>
+     <button className={styles.btn2} onClick={}>Preparing</button>
+     <button className={styles.btn3} onClick={}>Ready for delivery</button>
+     <button className={styles.btn4} onClick={}>Delivering</button>
+     <button className={styles.btn5} onClick={}>Delivered</button>
 
         </div>
     )
